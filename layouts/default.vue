5bbbeebc5fac3a69bd67da47246231b57e8ffaf7
Taro <tarohiraishi1@gmail.com>
2024-06-25 00:49:18 +0900

    サイドメニューの画面線を実装しよう

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,15 +31,16 @@ import {
   NMenu,
   type MenuOption,
 } from "naive-ui";
+import { RouterLink } from "vue-router";
 
 const menuOptions: MenuOption[] = [
   {
     key: "users",
-    label: "Users",
+    label: () => h(RouterLink, { to: "/users" }, { default: () => "Users" }),
   },
   {
     key: "posts",
-    label: "Posts",
+    label: () => h(RouterLink, { to: "/posts" }, { default: () => "Posts" }),
   },
 ];
 </script>
