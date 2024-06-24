64eda2b6c0bb1a00f179fb0c89aed5cc153f6f50
Taro <tarohiraishi1@gmail.com>
2024-06-24 23:56:12 +0900

    useFetchを使ってjsonplaceholderにリクエストしてみよう

---
+++ pages/users/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ JSON.stringify(users, null, 2) }}</div>
+</template>
+
+<script setup lang="ts">
+import type { User } from "~/models/user";
+
+const { data: users } = useFetch<User[]>(
+  "https://jsonplaceholder.typicode.com/users"
+);
+</script>
