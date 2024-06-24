769598e7b98543f555da7b9d1f3e77f74df2090c
Taro <tarohiraishi1@gmail.com>
2024-06-25 00:04:27 +0900

    取得したデータをテーブル形式で表示しよう

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>{{ JSON.stringify(users, null, 2) }}</div>
+  <NDataTable remote v-if="users" :data="users" :columns="columns" />
 </template>
 
 <script setup lang="ts">
+import { NDataTable, type DataTableColumns } from "naive-ui";
 import type { User } from "~/models/user";
 
 const { data: users } = useFetch<User[]>(
   "https://jsonplaceholder.typicode.com/users"
 );
+
+const columns = computed<DataTableColumns<User>>(() => [
+  {
+    key: "id",
+    title: "ID",
+    render: (row) => row.id,
+  },
+  {
+    key: "name",
+    title: "Name",
+    render: (row) => row.name,
+  },
+  {
+    key: "email",
+    title: "Email",
+    render: (row) => row.email,
+  },
+  {
+    key: "website",
+    title: "Website",
+    render: (row) => row.website,
+  },
+]);
 </script>
