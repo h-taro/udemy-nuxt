092b3488611d497e6c6b80f40f0101edaae2486a
Taro <tarohiraishi1@gmail.com>
2024-06-25 17:07:12 +0900

    ユーザーの詳細情報を取得しよう

--- pages/users/[id]/index.vue
+++ pages/users/[id]/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <pre>{{ JSON.stringify(user, null, 2) }}</pre>
+  </div>
 </template>
+
+<script setup lang="ts">
+import type { User } from "~/models/user";
+
+const api = useApi();
+const route = useRoute();
+
+const userId = computed<string>(() =>
+  decodeURIComponent(String(route.params.id))
+);
+
+const { data: user } = useAsyncData<User>(() => api(`/users/${userId.value}`));
+</script>
