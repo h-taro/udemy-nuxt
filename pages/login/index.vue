6c63c8aa5ee2084dc3c3d9d1ffde87333e93c661
Taro <tarohiraishi1@gmail.com>
2024-06-25 11:53:29 +0900

    入力した内容からユーザーの情報を取得しよう

---
+++ pages/login/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <NForm ref="formRef" :rules="rules" :model="formValue">
+    <div class="flex flex-col gap-[24px]">
+      <NFormItem label="ID" path="id">
+        <NInput v-model:value="formValue.id" />
+      </NFormItem>
+      <NFormItem label="パスワード" path="password">
+        <NInput v-model:value="formValue.password" :type="'password'" />
+      </NFormItem>
+      <NButton :attr-type="'submit'" @click="login">
+        <span class="text-[14px]">ログイン</span>
+      </NButton>
+    </div>
+  </NForm>
+</template>
+
+<script setup lang="ts">
+import {
+  NButton,
+  NForm,
+  NFormItem,
+  NInput,
+  type FormInst,
+  type FormRules,
+} from "naive-ui";
+import type { User } from "~/models/user";
+
+definePageMeta({
+  layout: "static",
+});
+
+interface FormValue {
+  id: string;
+  password: string;
+}
+const emptyFormValue: FormValue = {
+  id: "",
+  password: "",
+};
+const formValue = ref<FormValue>(emptyFormValue);
+const formRef = ref<FormInst>();
+const rules: FormRules = {
+  id: {
+    required: true,
+    message: "IDを入力してください",
+  },
+  password: {
+    required: true,
+    message: "パスワードを入力してください",
+  },
+};
+
+const api = useApi();
+
+const login = async () => {
+  try {
+    await formRef.value?.validate();
+
+    const res = await api<User>(`/users/${formValue.value.id}`);
+    console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
