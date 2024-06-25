34e80f18513b474d359d8b1721e108b9655bb904
Taro <tarohiraishi1@gmail.com>
2024-06-25 17:18:58 +0900

    ユーザーの詳細画面の見た目を調整しよう

--- pages/users/[id]/index.vue
+++ pages/users/[id]/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
-    <pre>{{ JSON.stringify(user, null, 2) }}</pre>
+  <div class="px-[40px] py-[32px] flex flex-col gap-[32px]">
+    <div class="flex flex-row items-center justify-between">
+      <span class="text-[24px] font-bold">{{ user?.name }}</span>
+      <div class="flex flex-row items-center gap-[12px]">
+        <NButton>
+          <span class="text-[14px]">編集</span>
+        </NButton>
+        <NButton>
+          <span class="text-[14px]">編集</span>
+        </NButton>
+      </div>
+    </div>
+    <NTabs>
+      <NTabPane name="basic"> </NTabPane>
+      <NTabPane name="posts"> </NTabPane>
+      <NTabPane name="albums"> </NTabPane>
+      <NTabPane name="todos"> </NTabPane>
+    </NTabs>
   </div>
 </template>
 
 <script setup lang="ts">
+import { NButton, NTabPane, NTabs } from "naive-ui";
 import type { User } from "~/models/user";
 
 const api = useApi();
