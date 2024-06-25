3c5f517ef9b5e245e32997bbbeb80ca2951e95c6
Taro <tarohiraishi1@gmail.com>
2024-06-26 01:07:03 +0900

    ユーザー新規登録のフォームのUIを作成しよう

--- pages/users/new/index.vue
+++ pages/users/new/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="px-[40px] py-[32px] flex flex-col gap-[32px]">
     <span class="text-[24px] font-bold">新規登録</span>
+    <SectionUserForm />
+    <div class="flex flex-row items-center justify-end">
+      <NButton>
+        <span class="text-[14px]">新規登録</span>
+      </NButton>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { NButton } from "naive-ui";
+</script>
